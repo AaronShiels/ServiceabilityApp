e4458aa855d111fd0f8d6016021851ce2550547c
Aaron Shiels <aaron.shiels@outlook.com>
2017-06-24 23:32:32 +1000

    Fleshed out Financials section. Added test data.

--- src/ServiceabilityApp.Api/Client/app/assess/assess.component.js
+++ src/ServiceabilityApp.Api/Client/app/assess/assess.component.js
@@ -11,6 +11,17 @@ function assessComponent() {
     function assessController() {
         var ctrl = this;
 
-        ctrl.entities = [{}];
+        ctrl.entities = [
+            {
+                firstName: 'Aaron',
+                lastName: 'Shiels',
+                dob: new Date(1989,03,28)
+            },
+            {
+                firstName: 'William',
+                lastName: 'Farrell',
+                dob: new Date(1989, 11, 22)
+            },
+        ];
     }
 }
