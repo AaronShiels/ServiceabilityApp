ef9ac387f15063b90cbd1c7b33c0c10d50304813
Aaron Shiels <aaron.shiels@outlook.com>
2018-01-04 22:36:26 +1000

    A complete working React build pipeline.

---
+++ src/ServiceabilityApp.Client/build-scripts/move-to-wwwroot.js
@@ -0,0 +1,12 @@
+﻿const fs = require('fs-extra');
+
+const src = 'build';
+const dest = '../ServiceabilityApp.Api/wwwroot';
+
+fs.emptyDir(dest)
+    .then(() => console.log('Destination directory cleared successfully.'))
+    .catch(error => console.error(error));
+
+fs.copy(src, dest)
+    .then(() => console.log('Source contents moved to destination directory successfully.'))
+    .catch(error => console.error(error));
